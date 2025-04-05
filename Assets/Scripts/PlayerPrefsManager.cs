fa0987fe8ccf0b473e89910d0897caff91259b84
NhuQuyen <nhuquyen18082001@gmail.com>
2025-04-05 11:44:00 +0700

    Commit

---
+++ Assets/Scripts/PlayerPrefsManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPrefsManager
+{
+   public const string Coin = "COIN";
+   public const string Hint = "HINT";
+   public const string Shuffle = "SHUFFLE";
+   public const string LevelUnlock = "LEVELUNLOCK";
+   public const string isGetDefaultItems = "ISGETDEFAULTS";
+   public const string levelUnlock = "LEVELUNLOCK";
+   public const string freeze =  "FREEZE";
+}
